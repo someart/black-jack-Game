5838330d26499706ec108a5dacce2911e124abd4
someart <maissamth@gmail.com>
2023-09-22 05:11:22 +0100

    basics of js code

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 4
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
 
+console.log(message)
