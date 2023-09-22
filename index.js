1035b9a9d47ccdf22986dbda00609363e86bebe6
someart <maissamth@gmail.com>
2023-09-22 23:39:34 +0100

    array test

--- index.js
+++ index.js
@@ -37,3 +37,20 @@ function newCard() {
     // 3. Call startGame()
     renderGame()
 }
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear"
+]
+
+// DRY - Don't repeat yourself
+console.log(messages[0])
+console.log(messages[1])
+console.log(messages[2])
+console.log(messages[3])
+
+
+
+
